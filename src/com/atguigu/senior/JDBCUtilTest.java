47aadf8ae29b440bce99615b53b396da87476866
RollsRoycewk <50765560@qq.com>
2024-08-02 23:14:37 +0800

    feat：工具类封装V2.0

--- src/com/atguigu/senior/JDBCUtilTest.java
+++ src/com/atguigu/senior/JDBCUtilTest.java
@@ -1,6 +1,7 @@
 package com.atguigu.senior;
 
 import com.atguigu.senior.util.JDBCUtil;
+import com.atguigu.senior.util.JDBCUtilV2;
 import org.junit.Test;
 
 import java.sql.Connection;
@@ -22,5 +23,28 @@ public class JDBCUtilTest {
 
         //CRUD
         JDBCUtil.release(connection);
+    }
+
+    @Test
+    public void testJDBCV2() {
+        /*
+
+        Connection connection = JDBCUtil.getConnection();
+        Connection connection1 = JDBCUtil.getConnection();
+        Connection connection2 = JDBCUtil.getConnection();
+
+        不同的三个连接
+        System.out.println(connection);
+        System.out.println(connection1);
+        System.out.println(connection2);
+        */
+
+        // 是同一个连接
+        Connection connection1 = JDBCUtilV2.getConnection();
+        Connection connection2 = JDBCUtilV2.getConnection();
+        Connection connection3 = JDBCUtilV2.getConnection();
+        System.out.println(connection1);
+        System.out.println(connection2);
+        System.out.println(connection3);
     }
 }
